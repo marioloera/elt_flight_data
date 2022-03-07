094ff29d4793cbfb01167b242aab5358b23e8041
Mario Loera <mario.loera@trustly.com>
2022-03-07 17:34:24 +0100

    add run_etl_flights_taskflow.sh

---
+++ scripts/run_etl_flights_taskflow.sh
@@ -0,0 +1,12 @@
+# airflow dags list
+
+airflow tasks list -t etl_flights_taskflow
+
+# since the task use previus task returns values
+# they need to be executed in order
+
+airflow tasks test etl_flights_taskflow get_airports 20220301
+
+airflow tasks test etl_flights_taskflow get_flights_per_country 20220301
+
+airflow tasks test etl_flights_taskflow write_to_csv 20220301
